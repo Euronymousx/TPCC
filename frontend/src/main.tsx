76d63b41e071503469fc30b1f0e3a0313c8c6851
Euronymousx <35211433+Euronymousx@users.noreply.github.com>
2025-06-05 15:17:41 -0400

    chore: remove screenshots and update README

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { polyfill } from 'smoothscroll-polyfill'
+
+polyfill()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
